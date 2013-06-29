3ddba58ef031cba46cea3ff7302ec9bdf538c3b5
Ross Donaldson <gastove@gmail.com>
2013-06-28 23:36:15 -0700

    A little more re-organizing. Modifying DigestHTML to return multiple formats. Nothing exciting to see here yet.

--- src/main/scala/ml/DigestHTML.scala
+++ src/main/scala/ml/DigestHTML.scala
@@ -15,25 +15,32 @@ import java.net.URL
  * @returns List[String]
 */
 
-class DigestHTML(filePath: String) {
+object DigestHTML {
 
-  val textHeap = loadAndParseFile(filePath)
+  def processFileToList(filePath: String): List[String] = loadAndParseFile(filePath)
 
-  private def loadAndParseFile(filePath: String): List[List[String]] = {
+  def processFileToListOfLists(filePath: String): List[List[String]] = loadAndParseFile(filePath).map(_.split("\n").toList).toList
+
+  private def loadAndParseFile(filePath: String): List[String] = {
     // This function... well, ideally will work for all Blogspot blogs. Hrm.
 //    val rootNode = cleaner.clean(new URL(urlToClean).openStream)
     val cleaner = new HtmlCleaner
     val rootNode = cleaner.clean(new File(filePath) )
-    rootNode
+    val postBody = rootNode
       .getElementsByName("div", true)
       .map{ elem =>
-      elem.getAttributeByName("class") match {
-        case "post" | "post-body" => elem.getText.toString.replaceAll("&nbsp;", "")
-        case _ => } }
+        elem.getAttributeByName("class") match {
+          case "post" | "post-body" => elem.getText.toString.replaceAll("&nbsp;", "")
+          case _ =>
+        }
+      }
       .toList
-      .foldLeft(List[List[String]]())( (acc, entry) => entry match {
-        case entry:String => (entry split("\n") toList) :: acc
-        case entry:Any => acc } )
+    postBody.foldLeft(List[String]())(
+      (acc, entry) => entry match {
+        case entry: String => entry :: acc
+        case entry: Any => acc
+      }
+    )
   }
 
 }
